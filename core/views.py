3cb32ab19b36b67f30b836f4959499cc300c148c
Dollyn-boy <vitordoreahdx11@gmail.com>
2025-07-19 12:21:05 -0300

    Início da criação da base.html

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import HttpResponse # Importe HttpResponse se for usá-lo em algum lugar
+from orders.models import Order, OrderItem
+
+# O decorador @login_required foi removido desta view
+def home(request):
+    return render(request, 'core/base.html')
--- dgust_site/urls.py
+++ dgust_site/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
+    path('home/', include('core.urls')),
     path('order/', include('orders.urls')),
     path('products/', include('products.urls')),
     path('combos/', include('combos.urls')),
